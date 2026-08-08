7be03c15d88b4c2b6fb5d97a4ec2c7ac3a06ce40
SergeiKirillov <sergei.kirillov.a@gmail.com>
2026-08-08 20:34:17 +0500

    08.08.2026 20:34

--- shift12h/src/shift12h/app.py
+++ shift12h/src/shift12h/app.py
@@ -26,7 +26,7 @@ class Shift12H(toga.App):
              placeholder="dd.mm.yyyy",
              style=Pack(
                  width=100,
-                 padding=(0, 8),
+                 margin=(0, 8),
                  ),
         )
         self.txtDataSelection.on_confirm=self.on_confirm
@@ -60,21 +60,21 @@ class Shift12H(toga.App):
             on_press=self.btnPosle_press,
         )
 
-        lbl1smena = toga.Label("1 смена: 19:00 - 7:00", style=Pack(padding_top=8),)      
-        lbl2smena = toga.Label("2 смена: 7:00 - 19:00", style=Pack(padding_top=8),)
-        self.lbl1smena_brigada = toga.Label("_", style=Pack(padding_top=8),)
-        self.lbl2smena_brigada = toga.Label("_", style=Pack(padding_top=8),)
+        lbl1smena = toga.Label("1 смена: 19:00 - 7:00", style=Pack(margin_top=8),)      
+        lbl2smena = toga.Label("2 смена: 7:00 - 19:00", style=Pack(margin_top=8),)
+        self.lbl1smena_brigada = toga.Label("_", style=Pack(margin_top=8),)
+        self.lbl2smena_brigada = toga.Label("_", style=Pack(margin_top=8),)
         spacer = toga.Box(style=Pack(flex=1))
         
          # Верхняя строка: дата + кнопка
-        date_row = toga.Box(style=Pack(direction=ROW, alignment=CENTER, padding_bottom=10))
-        date_row.add(toga.Label("Дата:", style=Pack(width=60, padding_right=8)))
+        date_row = toga.Box(style=Pack(direction=ROW, align_items=CENTER, margin_bottom=10))
+        date_row.add(toga.Label("Дата:", style=Pack(width=60, margin_right=8)))
         date_row.add(self.txtDataSelection)
         date_row.add(self.date_button)
         date_row.add(self.btnToday)
 
         # Нижняя строка: кнопки навигации
-        nav_row = toga.Box(style=Pack(direction=ROW, alignment=CENTER, padding=(0,10,0,10)))
+        nav_row = toga.Box(style=Pack(direction=ROW, align_items=CENTER, margin=(0,10,0,10)))
         nav_row.add(self.btnDo)
         nav_row.add(self.btnPosle)
 
@@ -82,8 +82,8 @@ class Shift12H(toga.App):
         content = toga.Box(
             style=Pack(
                 direction=COLUMN,
-                padding=20,
-                alignment=CENTER,
+                margin=20,
+                align_items=CENTER,
                 flex=1
             )
         )
@@ -92,7 +92,7 @@ class Shift12H(toga.App):
         card = toga.Box(
             style=Pack(
                 direction=COLUMN,
-                padding=16,
+                margin=16,
                 flex=1
             )
         )
@@ -141,35 +141,19 @@ class Shift12H(toga.App):
     def on_confirm(self, widget):
         self.update_shift()    
 
-    
-      
-
     def set_today(self):
         today=datetime.today()
-        self.session.current_date = today
         self.txtDataSelection.value = today.strftime("%d.%m.%Y")
-
-        #night, day = self.shift.get_shift(today)
-        #self.lbl1smena_brigada.text = f"Бригада №{night}"
-        #self.lbl2smena_brigada.text = f"Бригада №{day}"
-        
-        
-
-
+        self.update_shift()
 
     def update_shift(self):
-        try:
-            current_date =datetime.strptime(self.txtDataSelection.value, "%d.%m.%Y").date()
-            #current_date =  self.txtDataSelection.get_date() # Возвращает None Если такой даты нет
+        current_date =  self.txtDataSelection.get_date() # Возвращает None Если такой даты нет
            
-
-        except ValueError:
+        if current_date is None:
             self.lbl1smena_brigada.text = "Ошибка даты"
-            self.lbl2smena_brigada.text = "Ошибка даты"
-            return
-
+            self.lbl2smena_brigada.text = "Ошибка даты" 
+            return    
         
-
         night, day = self.shift.get_shift(current_date)
         self.lbl1smena_brigada.text = f"Бригада №{night}"
         self.lbl2smena_brigada.text = f"Бригада №{day}"
--- shift12h/src/shift12h/ui/date_input.py
+++ shift12h/src/shift12h/ui/date_input.py
@@ -10,35 +10,39 @@ class DateInput(toga.TextInput):
 
     def on_date_change(self, widget):
 
-        value = self.value
+         #  if not self._updating:
+         #       return 
+         #  value = self.value
 
-        digits = "".join(
-             char for char in value
-             if char.isdigit()
-        )
+         #  # Оставляем только цифры  
+         #  digits = "".join(
+         #     char for char in value
+         #     if char.isdigit()
+         #  )
 
-        digits= digits[:8]
+         #  # Максимум 8 цифр: ДДММГГГГ  
+         #  digits= digits[:8]
 
-        parts=[]
+         #  # Формируем ДД.ММ.ГГГГ
+         #  if len(digits) >= 2:
+         #     formatted = digits
+         #  elif len(digits)<=4:
+         #     formatted = digits[:2] + "." + digits[2:]
+         #  else:
+         #       formatted = digits[:2] + "." + digits[2:4] + "." + digits[4:]
 
-        if len(digits) >= 2:
-             parts.append(digits[:2])
-        else:
-             parts.append(digits)
+         #  # Если значение действительно изменилось     
+         #  if formatted != value:
+         #     self._updating = True
+         #     try:
+         #        self.value = formatted
+         #     finally:
+         #         self._updating = False
+    
+         #     # у Toga другой механизм работы с текстовым полем
+         pass
 
-        if len(digits)>=4:
-             parts.append(digits[2:4])
-        elif len(digits)>2:
-             parts.append(digits[2:])
-
-        if len(digits)>4:
-             parts.append(digits[4:])
-
-        formatted=".".join(parts)
-
-        if formatted != value:
-             self.value = formatted
-
+             
 
     def get_date(self):        
          try:
